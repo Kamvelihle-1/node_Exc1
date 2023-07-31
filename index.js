525710aabbbf6cbdc830f7d74bf3cb3c2af5ff72
SmasherK <ksikalan@gmail.com>
2023-07-31 14:14:03 +0200

    done

---
+++ index.js
@@ -0,0 +1,17 @@
+const {writeFile, readFile, appendFile} = require ('fs')
+
+writeFile('data.txt', 'Hello node file system','utf-8', (err) => {
+    if (err) 
+        console.log(err);
+    else
+        console.log('The file has been saved!');
+  });
+
+//When writing a second line to data.txt it overrides the previous line/content
+
+appendFile('data.txt',' Node.js for backend','utf-8',(err)=>{
+    if(err)
+        console.log(err);
+    else
+        console.log('File has been appended');
+})
